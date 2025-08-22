2c684855270f64534e1f8e0c77352c7152acfe86
Thiago Perin <thiagoperin2310@gmail.com>
2025-08-22 01:13:34 -0300

    feat: creates hasAuthorization middleware for authentication in the backend

--- server/src/middlewares.ts
+++ server/src/middlewares.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
+import AuthCookie from "./utils/AuthCookie";
 
-const forbiddenChars = "'\"<>():?/*\\;[]{}=+!%$#@~^s";
+const forbiddenChars = "'\"<>():?/*\\;[]{}=+!%$#@~^";
 
 function removeForbiddenChars(input: string) {
 	return String(input).split("").map(char => forbiddenChars.includes(char) ? "" : char).join("");
@@ -17,37 +18,10 @@ export function sanitizeInput(req: Request, _: Response, next: NextFunction) {
 	return next();
 }
 
-// #1º verifica se existeo atributo "company" no token.
-// Se não tiver, retorna false e da "Acesso negado".
-
-// #2º verifica se o comprimento do array "company" é igual a 1
-// caso nao seja igual a 1, significa que nenhum ou mais de um valor foi atribuido para "company"
-// nesso caso, retorna false e da "Acesso negado".
-
-// #3º após verificar se company existe e tem o tamanho correto de elementos (1)
-// é então verificado se o valor do atributo company é um CPF ou CNPJ válido
-// a verificação pode ser feita tanto com as pontuações (".", "-" e "/") quanto sem elas.
-// caso seja um CPF ou CNPJ válido, retorna true e permite o acesso
-// caso não seja um CPF ou CNPJ válido, retorna false e da "Acesso negado".
-// export const hasAuthorization: GuardFn = (token, _req) => {
-// 	const company = (token as any).content.company;
-// 	const name = (token as any).content.name;
-// 	const email = (token as any).content.email;
-// 	const username = (token as any).content.preferred_username;
-// 	if (company && name && email && username) {
-// 		if (typeof company === "string") {
-// 			return cpf.isValid(company) || cnpj.isValid(company);
-// 		} else if (Array.isArray(company) && company.length === 1) {
-// 			return cpf.isValid(company[0]) || cnpj.isValid(company[0]);
-// 		} else {
-// 			return false;
-// 		}
-// 	} else {
-// 		return false;
-// 	}
-// };
-
-// export const hasAuthorization: GuardFn = (token, _req) => {
-// 	return true
-// 		|| token.hasApplicationRole(appName, "Admin");
-// }; 
+export function hasAuthorization(req: Request, res: Response, next: NextFunction) {
+	if ((req.cookies?.["_bta"] || "") === AuthCookie.getDaily(new Date())) {
+		return next();
+	} else {
+		return res.status(401).redirect("/login");
+	}
+}
