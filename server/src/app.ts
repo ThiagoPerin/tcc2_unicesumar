cc52ffbcd7ff2b45b2bebf48ce970a737f1cf1ad
Thiago Perin <thiagoperin2310@gmail.com>
2025-08-22 01:14:21 -0300

    feat: applies simple auth in the app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,10 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import apiRouter from "./api/router";
 import helmet from "helmet";
+import { hasAuthorization } from "./middlewares";
+import loginRouter from "./router";
 
 const app = express();
-
-app.set("trust proxy", 1);
 
 if (process.env["NODE_ENV"] !== "development") {
     app.use(helmet());
@@ -15,8 +15,10 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api/v1", apiRouter);
 app.get("/", (_, res) => { res.redirect("/app"); });
+app.use("/login", loginRouter);
+app.use("/app", [hasAuthorization]);
+app.use("/api/v1", [hasAuthorization], apiRouter);
 app.use(express.static("static"));
 
 export default app;
