d1ceb9e8279fb09319cc40578784d7938cc4e24c
Thiago Perin <thiagoperin2310@gmail.com>
2025-08-26 23:36:24 -0300

    feat: improves the login page ui

--- ui/login/src/Login.svelte
+++ ui/login/src/Login.svelte
@@ -1,10 +1,7 @@
-<script>
-
-</script>
-
 <div class="content">
 	<div class="areaForm">
-		<form id="form_login" action="/login" method="POST">
+		<img src="/images/lupulo-branco.png" alt="Axon" id="logo">
+		<form id="form_login" action="/auth/login" method="POST">
 			<div class="inputDiv form-group">
 				<label for="user" class="form-label">Usuário</label>
 				<input type="text" name="user" class="form-control bg-dark text-white" id="user" placeholder="Digite seu usuário..." required>
@@ -18,11 +15,6 @@
 			</div>
 		</form>
 	</div>
-
-	<div class="areaBrands">
-		<img src="/images/bodebrownWhiteLogo.png" alt="Bodebrown" id="logoBode">
-		<img src="/images/axonIDhorizontalwhite.png" alt="Axon" id="logoAxon">
-	</div>
 </div>
 
 <style>
@@ -32,98 +24,50 @@
 		background: radial-gradient(circle at 24.1% 68.8%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%);
 		display: flex;
 		flex-direction: row;
-		justify-content: flex-start;
+		justify-content: center;
 		align-items: center;
-		padding: 60px;
+		padding: 10px;
 	}
 
 	.areaForm {
 		height: 100%;
-		width: 40%;
+		width: 100%;
 		padding: 40px;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: space-evenly;
+		justify-content: center;
+		gap: 20px;
+	}
+
+	#logo {
+		width: 100%;
+		max-width: 200px;
 	}
 
 	form {
 		width: 100%;
-		height: 50%;
+		max-width: 300px;
+		height: fit-content;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: space-around;
+		justify-content: center;
+		gap: 10px;
 	}
 
 	.inputDiv {
-		margin: 10px 0px;
 		height: fit-content;
 		width: 100%;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: flex-start;
-	}
-
-	.inputDiv button {
-		width: 100%;
-	}
-	.inputDiv input {
-		width: 100%;
-		/* border: none; */
-	}
-	.inputDiv Label {
 		color: #fff;
 		font-size: 20px;
 	}
 
-	.areaBrands {
-		height: 100%;
-		width: 60%;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-evenly;
-		align-items: center;
-	}
-	#logoBode {
-		width: 80%;
-	}
-
-	#logoAxon {
-		width: 70%;
-	}
-
-		@media screen and (max-width: 800px){
-		.content {
-			height: 100vh;
-				width: 100vw;
-				background: radial-gradient(circle at 24.1% 68.8%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%);
-				display: flex;
-				flex-direction: column-reverse;
-				justify-content: center;
-				align-items: center;
-				padding: 60px;
-		}
-
-		.areaBrands {
-		height: fit-content;
+	.inputDiv input, .inputDiv button {
 		width: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-evenly;
-		align-items: center;
-	}
-
-	.areaForm {
-		height: fit-content;
-		width: 100%;
-		padding: 40px;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: space-evenly;
-		/* background-color: darkred; */
-	}
 	}
 </style>
